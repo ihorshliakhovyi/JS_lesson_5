7dc5abb96441e4d2d88598b8f31cf663f835e113
ihorshliakhovyi <ihorshliakhovyi@gmail.com>
2022-11-24 15:45:57 +0200

    Task_1_Lesson_5

---
+++ js/index1.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// Задание 1
+// Напиши функцию-конструктор Account, которая создает объект со свойствами login и email. 
+// В prototype функции-конструктора добавь метод getInfo(), 
+// который выводит в консоль значения полей login и email объекта который его вызвал.
+
+const Account = function (login, email) {
+    this.login = login;
+    this.email = email;
+};
+Account.prototype.getInfo = function() {
+    console.log(`Login: ${this.login}, Email: ${this.email}`) 
+};
+
+// console.log('acc proto:', Account.prototype);
+
+const mango = new Account('Mangozedog', 'mango@dog.woof');
+
+// console.log(mango);
+mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
+
+const poly = new Account('Poly', 'poly@mail.com');
+
+poly.getInfo(); // Login: Poly, Email: poly@mail.com
